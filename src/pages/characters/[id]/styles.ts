e6301d47f152e86eab32cfa4d74a8d87a966c393
Mornieur <mariafernanda-coro@hotmail.com>
2022-08-07 03:40:00 -0300

    style: adding styling to the trait cards.

--- src/pages/characters/[id]/styles.ts
+++ src/pages/characters/[id]/styles.ts
@@ -1,16 +1,15 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100vw;
   width: 100%;
-  height: 100vh;
   height: 100%;
   display: grid;
   place-items: center;
+  background-color: black;
+  background-image: url('/fundoEstrelado.jpg');
 `
 
 export const Profile = styled.div`
-  background-color: red;
   max-width: 700px;
   width: 100%;
   max-height: 700px;
@@ -18,25 +17,71 @@ export const Profile = styled.div`
   display: grid;
 
   place-items: center;
+
   align-items: center;
   text-align: center;
+
+  position: relative;
+  top: -5rem;
 `
 
-export const ProfileImage = styled.div``
+export const ProfileImage = styled.div`
+  display: grid;
+  place-items: top normal;
+
+  section {
+    width: 400px;
+    height: 400px;
+
+    background-image: url('/portalTwo.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    display: grid;
+    place-items: center;
+  }
+  img {
+    width: 200px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    border-radius: 50%;
+    border: 3px solid white;
+  }
+`
 
 export const ProfileDetails = styled.div`
+  h2 {
+    width: 300px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+  }
+  text-align: center;
+  display: grid;
+  place-items: center;
+  flex: 1;
+  width: 700px;
+  //height: 300px;
+  /* background-color: yellow; */
+  color: white;
+  padding: 1rem 0;
+  strong {
+    max-width: 150px;
+    width: 100%;
+    height: 30px;
+    background-color: #00c84e;
+    border-radius: 10px;
+    text-align: center;
+    padding: 5px;
+  }
+`
+
+export const ProfileTitle = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
 
-  li {
-    list-style: none;
-    strong {
-      width: 250px;
-      height: 100px;
-      background-color: blue;
-    }
-  }
+  gap: 10px;
 `
