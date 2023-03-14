5dcb190616e99c58ce4f1ee4af8369a5423c172a
Mornieur <mariafernanda-coro@hotmail.com>
2023-03-14 16:40:52 -0300

    style: add new colors

--- src/global/styles/globals.js
+++ src/global/styles/globals.js
@@ -9,41 +9,30 @@ export const GlobalStyle = createGlobalStyle`
 
 }
 
+html{
+  width: 100vw;
+  overflow-x: hidden;
+}
 
+html, body {
+  height: 100vh;
+}
 
 body {
-  background-color: var(--white-100);
-
- 
   width:100%;
-  height:100%;
-
   scrollbar-width: thin;
+  font-family: 'Get Schwifty', sans-serif;
+  
 
+}
+
+h2 {
+    font-size: clamp(1.5rem, 8vw, 3.5rem);
 }
 
 
   scroll-behavior: smooth;
   overflow-x: hidden;
-/* 
-   ::-webkit-scrollbar {
-    width: 7px;
-    border-radius: 4px;
-  } 
-
-
-
-::-webkit-scrollbar-track {
-    background: grey;
-    background-color: var(--grey-boxShadow);
-    border-radius: 4px;
-  } 
-
-
-  ::-webkit-scrollbar-thumb {
-    background: grey;
-    border-radius: 4px;
-  } */
   
 
 
@@ -53,6 +42,7 @@ body {
 
   --black: #000;
   --white: #fff;
+  --white-50: #F4F4F4;
   --white-100: #f8f8f8;
 
   // Grey Colors 
@@ -60,8 +50,13 @@ body {
   
   --grey-100: #dddddd;
   --grey-400: #8a8a8a;
+  --grey-450: #868686;
+  --grey-470: #7C7C7C;
+  --grey-480: #7D7D7D;
   --grey-500: #797979;
   --grey-800: #4b4b4b;
+  --grey-850: #373737;
+  --grey-900: #2d2d2d;
 
   --grey-boxShadow: #c2c2c2;
   --grey-p: #6e6d6d;
@@ -69,7 +64,7 @@ body {
   // Yellow Colors
 
   --yellow-500: #ffd706;
-  --yellow-600: #fdd100;
+  --yellow-600: #FFD010;
 
 
 
@@ -84,23 +79,39 @@ body {
   --blue-dark: #2a4999;
 
   --blueTwo-light: #b3dcff;
+  --darkRed-color: #980a0a;
 
+  
 
   --name-blue: #1f9aff;
 
   --blue-background: #f6faff;
 
---blue-card: #f6faff;
+  --blue-card: #f6faff;
 
 
   // Pink Colors
 
   --pink-300: #F9778D;
 
-  // font weight
+  // Font weight
 
   --regular: 400;
   --medium: 500;
   --semiBold: 600;
+
+// Font size - Mobile
+
+ --smallMob-font: 0.85rem;
+ --averageMob-font: 1rem;
+ --largeMob-font: 1.5rem;
+
+// Font size - Desktop
+
+ --smallDesk-font: 0.85rem;
+ --averageDesk-font: 1rem;
+ --highDesk-font: 1.3rem;
+ --largeDesk-font: 2rem;
+ --extraBigDesk-font: 3.5rem;
 }
 `
